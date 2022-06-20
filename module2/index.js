c97aae91c264d7a9b6b54b94bd795be403f93454
Bartłomiej Papis <72127774+papiS23@users.noreply.github.com>
2022-06-20 16:06:27 +0200

    saving data to database and getting the data back on client

--- module2/index.js
+++ module2/index.js
@@ -1,16 +1,31 @@
 const express = require("express");
+const Datastore = require("nedb");
 const app = express();
-app.listen(3000, () => console.log("Listening at port 3000"));
+
+app.listen(3000, () => console.log("Running at port 3000"));
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 
-app.post("/api", (request, response) => {
-  console.log(request.body);
-  const data = request.body;
-  response.json({
-    status: "succes",
-    latitude: data.lat,
-    longitude: data.lon,
+const database = new Datastore("database.db");
+database.loadDatabase();
+
+app.post("/send_data", (req, res) => {
+  const reqData = req.body;
+  const timestand = Date.now();
+  reqData.timestand = timestand;
+
+  database.insert(reqData);
+
+  reqData.status = "Succes";
+  res.json(reqData);
+});
+
+app.get("/get_data", (req, res) => {
+  database.find({}, (error, data) => {
+    if (error) {
+      res.end();
+    } else {
+      res.json(data);
+    }
   });
 });
-//TODO: save the response data to the array and check out the node.js file system.
